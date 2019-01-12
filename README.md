27719879e662dd9f2c356a81d464df33959c1fdf
michael <michael@n.c>
2019-01-12 09:44:49 +0000

    create distributable archive cde.tgz

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ Modify the generated applications:
 Dockerfile: Install your application adding it to the list of applications
 in apt-get install
 cde/creat.sh: Call you application prefixed with "cde "
+
+Other people have provided sets of tools for various platforms here,
+prebuilt as statically linked binaries including the build scripts:
+https://github.com/andrew-d/static-binaries
+https://github.com/ernw/static-toolbox
+
+
+ 
--- create-cde.sh
+++ create-cde.sh
@@ -3,11 +3,13 @@
 set -e
 
 tag=my-cde-apps
-docker rmi --force $tag
 name=N$(date +%s)
+rm -f cde.tgz 2>/dev/null
+docker rmi --force $tag 2>/dev/null
 docker build -t $tag .
 #docker run --name $name --mount src="$(pwd)",target=/cde,type=bind -d $tag /bin/bash
 #docker run --name $name --cap-add SYS_PTRACE --mount src="$(pwd)/cde",target=/cde,type=bind  -t $tag /bin/bash -c 'cd /cde; ./refresh.sh; exit'
 docker run --name $name --cap-add SYS_PTRACE --mount src="$(pwd)/cde",target=/cde,type=bind -d -t $tag /bin/bash -c 'cd /cde; ./create.sh; exit'
 docker rm --force $name
 docker rmi --force $tag
+tar cfvz cde.tgz --exclude='cde/*sh' cde
